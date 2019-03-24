a63caeb442d8a0236e4adf2df84c4044d003842f
UVvirus <yuvasyuvarajan@gmail.com>
2019-03-24 10:54:57 +0530

    word count

--- src/Main.java
+++ src/Main.java
@@ -1,20 +1,16 @@
 import java.util.*;
 
 public class Main
-{
+{ int i;
 	public static void main(String[] args)
-	{
-		System.out.println("Hello World!");
-
-		Scanner input = new Scanner(System.in);
-
-		System.out.print("Enter a number: ");
-		double number1 = input.nextDouble();
-
-		System.out.print("Enter second number: ");
-		double number2 = input.nextDouble();
-
-		double product = number1 * number2;
-		System.out.printf("The product of both numbers is: %f", product);
-	}
+	{int word=1;
+	String str="count no of words";
+	for(int i=0;i<str.length();++i){
+		if(str.charAt(i)==' ')
+			word++;
+		
+	
+	}System.out.println("no of words= "+word);
+		System.out.println("no of space= "+(word-1));
+		}
 }
